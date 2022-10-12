3bea93b20961038ab46d194e377ad11cfff7fb84
laz0rde <pentester13377@gmail.com>
2022-10-12 14:00:26 +0200

    C# Implementation

---
+++ ArrayBasedStack/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class ArrayStack<T>
+    {
+        private int top;
+        private int MAXSTACK;
+        private T[] stackEntry;
+
+        public ArrayStack(int MAXSTACK)
+        {
+            this.MAXSTACK = MAXSTACK;
+            this.top = 0;
+            stackEntry = new T[MAXSTACK];
+        }
+
+        public void Push(T entry)
+        {
+            stackEntry[top++] = entry;
+        }
+
+        public T Pop()
+        {
+            return stackEntry[--top];
+        }
+
+        public int StackFull()
+        {
+            return (top == MAXSTACK) ? 1 : 0;
+        }
+
+        public int StackEmpty()
+        {
+            return (top == 0) ? 1 : 0;
+        }
+
+        public int StackSize()
+        {
+            return top;
+        }
+
+        public T StackTop()
+        {
+            return stackEntry[top-1];
+        }
+
+        public void ClearStack()
+        {
+            top = 0;
+        }
+    }
+}
---
+++ LinkedBasedStack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedBasedArray
+{
+    class StackNode<T>
+    {
+        public T entry { get; internal set; }
+        public StackNode<T> next { get; set; }
+    }
+    class LinkedStack<T>
+    {
+        StackNode<T> node;
+
+        public LinkedStack()
+        {
+            node = null;
+        }
+        public void Push(T entry)
+        {
+            StackNode <T> newnode = new StackNode<T>();
+            newnode.entry = entry;
+            newnode.next = node;
+            node = newnode;
+        }
+
+        public T Pop()
+        {
+            T ele = node.entry;
+            node = node.next;
+            return ele;
+        }
+
+       public T Peek()
+        {
+            return node.entry;
+        }
+
+
+    }
+}
